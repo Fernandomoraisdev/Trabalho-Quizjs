29816e46c1689efa52ceb130745615f55fd29294
FernandoDev31 <71509217+Fernandomoraisdev@users.noreply.github.com>
2020-12-03 20:13:54 -0300

    Imagens
    As imagens estão soltas, mas é só criar uma pasta chamada Imagens e jogar elas dentro e abrir todos os arquivos no editor.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Javascript-Quiz
+Um questionário online simples, dinâmico e amigável de tema aleatório.
+
+Tecnologias: Javascript, JSON, HTML, CSS, Bootstrap
---
+++ json.js
@@ -0,0 +1,14 @@
+data = {"quizcontent": [
+    {"question": "Quantos membros a banda Parasitii tem?","image": "Imagens/intr1.png",  "correct": 2, "a1":"2", "a2":"3","a3":"4","a4":"5"},
+    {"question": "Qual é o nome verdadeiro do Freakadadisk?","image": "Imagens/intr2.jpg", "correct": 3, "a1":"Catalin Stefan Ion", "a2":"Bogdan Ionut Pastaca","a3":"Petre Urda","a4":"Vlad Irimia"},
+	{"question": "O que o Cheloo quer dar?", "image": "Imagens/intr3.jpg", "correct": 3, "a1":"Dinheiro para os pobres", "a2":"Soco","a3":"dinheiro falso de esmola","a4":"Bebidas"},
+	{"question": "Onde Ombladon dedica essa balada para nós?","image": "Imagens/intr4.jpg", "correct": 1, "a1":"Na esquina do quarteirão", "a2":"Na Piscina","a3":"Na praia","a4":"Dentro de casa"},
+    {"question": "Quem eu acho que te contou sobre mim?","image": "Imagens/intr5.jpg",  "correct": 4, "a1":"Robson", "a2":"Thiago","a3":"Renato","a4":"Ninguém"},
+    {"question": "Qual meu sobrenome?","image": "meta/intr6.jpg","correct": 2, "a1":"Alves", "a2":"Morais","a3":"Ferreira","a4":"Nenhuma das opções"},
+	{"question": "O que mais você pode fazer com 2 palmas?", "image": "Imagens/intr7.jpg","correct": 1, "a1":"Bater", "a2":"Correr","a3":"Gritar","a4":"Nada"},
+	{"question": "Das 10 vibrações, quantas são negativas?","image": "Imagens/intr8.jpg", "correct": 4, "a1":"Nenhum", "a2":"2","a3":"3.14","a4":"9"},
+    {"question": "Que tipo de sinais os Parasitas dão para você vê-los de longe?", "image": "Imagens/intr9.jpg", "correct": 3, "a1":"SMS", "a2":"Grita?","a3":"Sinais de Fumaça","a4":"Acenam"},
+    {"question": "Quem descobriu o Brasil","image": "Imagens/intr10.jpg", "correct": 2, "a1":"Saci", "a2":"Pedro Alveres Cabral","a3":"Crivela","a4":"Patrique"}
+    ],
+	
+};
